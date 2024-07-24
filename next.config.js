2d385156c809280e3220fab390c17a1a3e4873f8
brendaAndersen <52722720+brendaAndersen@users.noreply.github.com>
2024-07-24 11:42:53 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // output: 'export' ,
+  basePath: "/portifolio-front",
   images: { unoptimized: true },
   experimental: {
     appDir: true,
