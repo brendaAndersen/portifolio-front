25c9df3d572da17dc93cd7b7690ded3fd12946bc
hosni mub <52722720+brendaAndersen@users.noreply.github.com>
2024-07-26 18:14:32 +0000

    updated styles

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,8 @@
-'use client'
+"use client";
 
 import { CharSection } from "./pages/home/home-section";
 
-//import { appWithTranslation } from 'next-intl';
-
 export default async function Home() {
-
   return (
     <>
       <CharSection />
--- app/pages/home/known-techs/index.tsx
+++ app/pages/home/known-techs/index.tsx
@@ -7,7 +7,7 @@ export const KnownTechs = () => {
   return (
     <section className="container py-16">
       <SectionTitle title={"Knowledges"} subtitle={"skills"} />
-      <div className="cursor-pointer w-full grid grid-cols-[repeat(auto-fit,minmax(264px,1fr))] gap-3 mt-[60px]">
+      <div className="cursor-pointer w-full grid sm:grid-cols-2 sm:p-2 small:p-2 small:grid-cols-2 lg:grid-cols-[repeat(auto-fit,minmax(264px,1fr))] gap-3 mt-[60px]">
         {techs.map((tech, index) => (
           <KnownTech
             key={index}
